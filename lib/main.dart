40d7bfa0e3295965ae99d12433e348fbdb79f3a0
sameeranand12 <201951134@iiitvadodara.ac.in>
2021-05-23 02:03:30 +0530

    Done

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+
+           //
+             child: Row(
+               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+               //
+
+               //crossAxisAlignment: CrossAxisAlignment.center,
+               children: [
+                 Container(height: 600.0, width: 100.0, color: Colors.yellow,),
+                 Row(
+                   children: [Column(
+                     mainAxisAlignment: MainAxisAlignment.center,
+                     children:[
+                       SizedBox(
+                         height: 200,
+                       ),
+                       Container(height: 100.0, width: 100.0, color: Colors.blue ,),
+                       Container(height: 100.0, width: 100.0, color: Colors.red ,),
+                       SizedBox(
+                         height: 190.0,
+                       ),
+                       Container(height: 10.0, width: 100.0, color: Colors.black ,),
+                     ],
+             ),
+              ],
+          ),
+                 Container(height: 600.0, width: 100.0, color: Colors.pinkAccent,),
+
+
+               ],
+             )
+
+
+
+                //SizedBox(
+                 // height: 300.0,
+               // ),
+
+          ),
+
+        ),
+      ),
+    ),
+  );
+}
